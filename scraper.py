7746ff4c24690411b6a71be69fee4b79a2957724
Sanika <Sanika>
2018-01-09 16:14:26 +0530

    provide functionality to write links to separate text files depending on language of trending repo

--- scraper.py
+++ scraper.py
@@ -1,14 +1,89 @@
+# generate time stamp 'ts'
+import datetime
+ts = datetime.datetime.now().strftime("%d-%m-%y-%H-%M-%S")
+
+# libs required for core scraping logic
 import requests
-from bs4 import BeautifulSoup, SoupStrainer
+from bs4 import BeautifulSoup
 
-page = requests.get("https://github.com/trending")
-response = requests.request('GET',"https://github.com/trending")
+# lib required for writing data to file in specific location
+from os import path
 
-pageSoup = BeautifulSoup(page.content, 'html.parser')
+# for java
+lang = "java"
+url_string = "https://github.com/trending?&since=weekly" + "&l=" + lang
+page = requests.get(url_string)
 
-for item in pageSoup.select("h3 a"):
-	tempItem = BeautifulSoup(item.get_text().replace('\n',' ') , 'html.parser')
-	tempStr = tempItem.get_text().replace(' ','')
-	repoLink = "https://github.com/"+tempStr+".git"
-	print(repoLink)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+file_path_string = lang + "/" + lang + ts + ".txt"
+file_path = path.relpath(file_path_string)
+file = open(file_path , "w+")
+
+for item in soup.select("h3 a"):
+	temp_item = BeautifulSoup(item.get_text().replace('\n',' ') , 'html.parser')
+	temp_str = temp_item.get_text().replace(' ','')
+	repo_link = "https://github.com/"+temp_str+".git"
+	file.write(repo_link + "\n")
 	
+file.close()
+
+
+# for c
+lang = "c"
+url_string = "https://github.com/trending?&since=weekly" + "&l=" + lang
+page = requests.get(url_string)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+file_path_string = lang + "/" + lang + ts + ".txt"
+file_path = path.relpath(file_path_string)
+file = open(file_path , "w+")
+
+for item in soup.select("h3 a"):
+	temp_item = BeautifulSoup(item.get_text().replace('\n',' ') , 'html.parser')
+	temp_str = temp_item.get_text().replace(' ','')
+	repo_link = "https://github.com/"+temp_str+".git"
+	file.write(repo_link + "\n")
+	
+file.close()
+
+
+# for cpp
+lang = "cpp"
+url_string = "https://github.com/trending?&since=weekly" + "&l=" + lang
+page = requests.get(url_string)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+file_path_string = lang + "/" + lang + ts + ".txt"
+file_path = path.relpath(file_path_string)
+file = open(file_path , "w+")
+
+for item in soup.select("h3 a"):
+	temp_item = BeautifulSoup(item.get_text().replace('\n',' ') , 'html.parser')
+	temp_str = temp_item.get_text().replace(' ','')
+	repo_link = "https://github.com/"+temp_str+".git"
+	file.write(repo_link + "\n")
+	
+file.close()
+
+
+# for csharp
+lang = "csharp"
+url_string = "https://github.com/trending?&since=weekly" + "&l=" + lang
+page = requests.get(url_string)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+file_path_string = lang + "/" + lang + ts + ".txt"
+file_path = path.relpath(file_path_string)
+file = open(file_path , "w+")
+
+for item in soup.select("h3 a"):
+	temp_item = BeautifulSoup(item.get_text().replace('\n',' ') , 'html.parser')
+	temp_str = temp_item.get_text().replace(' ','')
+	repo_link = "https://github.com/"+temp_str+".git"
+	file.write(repo_link + "\n")
+	
+file.close()
