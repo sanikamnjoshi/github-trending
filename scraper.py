e6ce4feb6a895dfae9a654ac2dd8da74474f71dd
Sanika <Sanika>
2018-01-10 15:03:47 +0530

    turn scraping logic into a function to get rid of earlier code duplication

--- scraper.py
+++ scraper.py
@@ -9,81 +9,37 @@ from bs4 import BeautifulSoup
 # lib required for writing data to file in specific location
 from os import path
 
-# for java
-lang = "java"
-url_string = "https://github.com/trending?&since=weekly" + "&l=" + lang
-page = requests.get(url_string)
-
-soup = BeautifulSoup(page.content, 'html.parser')
-
-file_path_string = lang + "/" + lang + ts + ".txt"
-file_path = path.relpath(file_path_string)
-file = open(file_path , "w+")
-
-for item in soup.select("h3 a"):
-	temp_item = BeautifulSoup(item.get_text().replace('\n',' ') , 'html.parser')
-	temp_str = temp_item.get_text().replace(' ','')
-	repo_link = "https://github.com/"+temp_str+".git"
-	file.write(repo_link + "\n")
+def scrape(lang):
+	url_string = "https://github.com/trending?&since=weekly" + "&l=" + lang
+	page = requests.get(url_string)
 	
-file.close()
+	soup = BeautifulSoup(page.content, 'html.parser')
+	
+	file_path_string = lang + "/" + lang + ts + ".txt"
+	file_path = path.relpath(file_path_string)
+	file = open(file_path , "w+")
+	
+	for item in soup.select("h3 a"):
+		temp_item = BeautifulSoup(item.get_text().replace('\n',' ') , 'html.parser')
+		temp_str = temp_item.get_text().replace(' ','')
+		repo_link = "https://github.com/"+temp_str+".git"
+		file.write(repo_link + "\n")
+		
+	file.close()
 
+
+# for java
+scrape("java")
+print("Text file of Java trending repos created.")
 
 # for c
-lang = "c"
-url_string = "https://github.com/trending?&since=weekly" + "&l=" + lang
-page = requests.get(url_string)
-
-soup = BeautifulSoup(page.content, 'html.parser')
-
-file_path_string = lang + "/" + lang + ts + ".txt"
-file_path = path.relpath(file_path_string)
-file = open(file_path , "w+")
-
-for item in soup.select("h3 a"):
-	temp_item = BeautifulSoup(item.get_text().replace('\n',' ') , 'html.parser')
-	temp_str = temp_item.get_text().replace(' ','')
-	repo_link = "https://github.com/"+temp_str+".git"
-	file.write(repo_link + "\n")
-	
-file.close()
-
+scrape("c")
+print("Text file of C trending repos created.")
 
 # for cpp
-lang = "cpp"
-url_string = "https://github.com/trending?&since=weekly" + "&l=" + lang
-page = requests.get(url_string)
-
-soup = BeautifulSoup(page.content, 'html.parser')
-
-file_path_string = lang + "/" + lang + ts + ".txt"
-file_path = path.relpath(file_path_string)
-file = open(file_path , "w+")
-
-for item in soup.select("h3 a"):
-	temp_item = BeautifulSoup(item.get_text().replace('\n',' ') , 'html.parser')
-	temp_str = temp_item.get_text().replace(' ','')
-	repo_link = "https://github.com/"+temp_str+".git"
-	file.write(repo_link + "\n")
-	
-file.close()
-
+scrape("cpp")
+print("Text file of CPP trending repos created.")
 
 # for csharp
-lang = "csharp"
-url_string = "https://github.com/trending?&since=weekly" + "&l=" + lang
-page = requests.get(url_string)
-
-soup = BeautifulSoup(page.content, 'html.parser')
-
-file_path_string = lang + "/" + lang + ts + ".txt"
-file_path = path.relpath(file_path_string)
-file = open(file_path , "w+")
-
-for item in soup.select("h3 a"):
-	temp_item = BeautifulSoup(item.get_text().replace('\n',' ') , 'html.parser')
-	temp_str = temp_item.get_text().replace(' ','')
-	repo_link = "https://github.com/"+temp_str+".git"
-	file.write(repo_link + "\n")
-	
-file.close()
+scrape("csharp")
+print("Text file of C# trending repos created.")
